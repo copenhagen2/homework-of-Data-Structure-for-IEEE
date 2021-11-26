4dcf5bf21eaf841ea49063ed275c14c2a54caea8
copenhagen2 <copenhagen@sjtu.edu.cn>
2021-11-26 16:55:48 +0800

    update

---
+++ 6-1.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+struct node {
+    int val;
+    node *next = nullptr;
+    node *pre = nullptr;
+};
+
+class linkList {
+private:
+    node *root;
+    node *rear;
+public:
+    linkList() {
+        root = new node;
+        rear = root;
+    }
+
+    void add(int x) {
+        node *tmp = new node;
+        tmp->val = x;
+        rear->next = tmp;
+        tmp->pre = rear;
+        rear = tmp;
+    }
+
+    // 查找元素并把查到的元素放在表头
+    int find(int x) {
+        int count = 0;
+        int flag = 0;
+        node *p = root->next;
+        while (p) {
+            count++;
+            if (p->val == x) {
+                flag = 1;
+                break;
+            }
+            p = p->next;
+        }
+        if (flag) {
+            p->pre->next = p->next;
+            if (p->next) p->next->pre = p->pre;
+            p->next = root->next;
+            root->next->pre = p;
+            root->next = p;
+            p->pre = root;
+        }
+        return count;
+    }
+    ~linkList() {
+        node *tmp = root;
+        while (tmp->next) {
+            node *p = tmp->next;
+            delete tmp;
+            tmp = p;
+        }
+        delete tmp;
+    }
+};
+
+int main() {
+    int n, m, res = 0;
+    linkList list;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        list.add(x);
+        //cout << "add one elem." << endl;
+    }
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        int x;
+        cin >> x;
+        res += list.find(x);
+    }
+    cout << res;
+    return 0;
+}
---
+++ 6-2.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <cmath>
+#include <climits>
+#include <vector>
+using namespace std;
+
+struct node {
+    int val;
+    node *left = nullptr;
+    node *right = nullptr;
+    node(int x = 0) : val(x) {};
+};
+
+int min(int a, int b) {
+    return a > b ? b : a;
+}
+
+class BySearchTree {
+private:
+    node *root;
+
+    void insert(int x, node *&point) {
+        if (!point) {
+            point = new node(x);
+            return;
+        }
+        if (x < point->val) insert(x, point->left);
+        else insert(x, point->right);
+    }
+
+    void remove(int x, node *&point) {
+        if (!point) return;
+        if (x < point->val) remove(x, point->left);
+        else if (x > point->val) remove(x, point->right);
+        else {
+            if (point->left && point->right) {
+                node *tmp = point->left;
+                while (tmp->right) tmp = tmp->right;
+                point->val = tmp->val;
+                remove(tmp->val, point->left);
+            }
+            else {
+                node *tmp = point;
+                point = point->left ? point->left:point->right;
+                delete tmp;
+                return;
+            }
+        }
+    }
+
+
+public:
+    BySearchTree() {
+        this->root = nullptr;
+    }
+
+    void insert(int x) {
+        insert(x, this->root);
+    }
+
+    void remove(int x) {
+        remove(x, this->root);
+    }
+
+    int inquire(int x) {
+        node *tmp = root;
+        int res = INT_MAX;
+        while (tmp) {
+            res = min(res, abs(x - tmp->val));
+            if (x == tmp->val) return 0;
+            if (x < tmp->val) tmp = tmp->left;
+            else tmp = tmp->right;
+        }
+        return res;
+    }
+};
+
+int main() {
+    int m;
+    vector<int> res;
+    cin >> m;
+    BySearchTree s;
+    for (int i = 0; i < m; i++) {
+        int op, x;
+        cin >> op >> x;
+        switch (op) {
+            case 0:
+                res.push_back(s.inquire(x));
+                break;
+            case 1:
+                s.insert(x);
+                break;
+            case 2:
+                s.remove(x);
+                break;
+        }
+    }
+    int n = res.size();
+    for (int i = 0; i < n; i++) {
+        cout << res[i] << endl;
+    }
+    return 0;
+}
